dfbea45d4116178c99973722ca1c25ba836e08fb
MENWITHTASTE <69464995+uthmantosin@users.noreply.github.com>
2024-11-21 05:45:19 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,8 +96,7 @@ resource "aws_instance" "foo" {
   subnet_id = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   availability_zone = "us-east-1a"
-  key_name = "devopskeypair"
-  count = 5
+  key_name = "mwtKP"
 
   tags = {
     Name = "server1"
