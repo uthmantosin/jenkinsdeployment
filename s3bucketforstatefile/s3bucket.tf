3e6deeeec90ecb22e8d818118b5cd5bba4b6da39
MENWITHTASTE <69464995+uthmantosin@users.noreply.github.com>
2024-12-13 04:32:07 -0800

    Update s3bucket.tf

--- s3bucketforstatefile/s3bucket.tf
+++ s3bucketforstatefile/s3bucket.tf
@@ -1,10 +1,13 @@
+# configured aws provider with proper credentials
 provider "aws" {
   region    = "us-east-1"
   profile   = "MENWITHTASTE"
 }
 
+
+
 resource "aws_s3_bucket" "example" {
-  bucket = "mwtbucket"
+  bucket = "menwithtaste-new"
 
   lifecycle {
     prevent_destroy = true
@@ -12,9 +15,25 @@ resource "aws_s3_bucket" "example" {
 }
 
 
+
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.example.id
   versioning_configuration {
     status = "Enabled"
+  }
+}
+
+
+
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "app-state"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
