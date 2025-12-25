6d347459897a16780ea7518c50662c65e58b2de8
MENWITHTASTE <69464995+uthmantosin@users.noreply.github.com>
2025-12-25 03:51:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region     = "us-east-1"
+  region    = "us-east-1"
+  profile   = "MENWITHTASTE"
 }
 
 # custom vpc
@@ -96,7 +97,7 @@ resource "aws_instance" "foo" {
   subnet_id = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   availability_zone = "us-east-1a"
-  key_name = "mwtKP"
+  key_name = "menwithtasteKPv2"
 
   tags = {
     Name = "server1"
