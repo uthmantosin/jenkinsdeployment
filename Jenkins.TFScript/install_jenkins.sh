f9cbdc12bc5e10e65c20c0e5d7a03e3781f1282f
MENWITHTASTE <69464995+uthmantosin@users.noreply.github.com>
2026-01-10 16:36:51 +0100

    Update install_jenkins.sh

--- Jenkins.TFScript/install_jenkins.sh
+++ Jenkins.TFScript/install_jenkins.sh
@@ -21,7 +21,7 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
     https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
     sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
-sudo apt install terraform -y
+sudo snap install terraform -y
 echo 'clearing screen...' && sleep 5
 clear
 echo 'jenkins is installed'
